f4a2eba3858da86396b8ab3bf74d7d10a599d2e8
f3man <stepan7box@gmail.com>
2023-02-03 14:46:29 +0200

    Update C# tasks.cs

--- C# tasks.cs
+++ C# tasks.cs
@@ -1,11 +1,10 @@
-
 // #1
 public class Program
 {
-  public static void Main()
+	public static void Main()
   {
     int b = 3;
-    ChangeValue(b, 5);
+   	ChangeValue(b, 5);
     Console.WriteLine(b);
   }
   
@@ -15,11 +14,61 @@ public class Program
   }
 }
 
-// #2
+// #2 List
+using System;
+using System.Collections.Generic;
+					
+public class Program
+{
+	public static void Main()
+	{
+		var addressList = new AddressRepository().GetAddresses();
+		var mapper = new AddressMapper();
+		var result = mapper.Map(addressList);
+	}
+}
 
+
+public class Address 
+{
+	public string Country { get; set;}
+	public string City { get; set;} 
+	public string Region { get; set;} 
+	public string StreetAddress { get; set;} 
+}
+
+public class AddressDto 
+{
+	public string Country { get; set; }
+	public string City { get; set; }
+	public string Region { get; set; }
+}
+
+public class AddressRepository 
+{
+	public List<Address> GetAddresses()
+	{
+		return new List<Address> {
+			new Address { Country = "USA", Region = "California", City = "LA", StreetAddress = "Boston st. 23" },
+			new Address { Country = "Ukraine", Region = "Zaporizka", City = "Zaporizhia", StreetAddress = "Ladozka 23" },
+			new Address { Country = "Poland", Region = "Lower Silesia", City = "Wroclaw", StreetAddress = "Legnicka 25" },
+		};
+	}
+}
+
+public class AddressMapper 
+{
+	public List<AddressDto> Map(ICollection<Address> addresses)
+	{
+		// Implement
+		throw new NotImplementedException("I should implement it");
+	}
+}
+
+// #3
 public static class Program
 {
-	public static Main()
+	public static void Main()
   {
     var actions = new List<Action>();
     for (var i = 0; i < 5; i++)
@@ -29,7 +78,7 @@ public static class Program
       });
     }
     
-    foreach (var actions in actions)
+    foreach (var action in actions)
     {
       action();
     }
@@ -47,11 +96,10 @@ public class Program
 	
 	private static HashSet<Address> getUniqueCities(List<Address> addresses)
 	{
-    var uniqueAddresses = new HashSet<Address>(new AddressUniqueCityComparer());
-    
 		throw new NotImplementedException("Implement me please");
 	}
 }
+
 
 
 public class Address 
@@ -62,17 +110,43 @@ public class Address
 	public string StreetAddress { get; set;} 
 }
 
+
 public class AddressRepository 
 {
 	public List<Address> GetAddresses()
 	{
 		return new List<Address> {
 			new Address { Country = "USA", Region = "California", City = "LA", StreetAddress = "Boston st. 23" },
-			new Address { Country = "USA", Region = "New York", City = "NY", StreetAddress = "Wall St. 23" },
-			new Address { Country = "Ukraine", Region = "Donetska", City = "NY", StreetAddress = "Peremogy St. 23" },
 			new Address { Country = "Ukraine", Region = "Zaporizka", City = "Zaporizhia", StreetAddress = "Ladozka 23" },
 			new Address { Country = "Ukraine", Region = "Zaporizka", City = "Zaporizhia", StreetAddress = "Borodinska 23" },
 			new Address { Country = "Poland", Region = "Lower Silesia", City = "Wroclaw", StreetAddress = "Legnicka 25" },
 		};
 	}
+}
+
+
+// #5
+public struct MegaStruct : IDisposable
+{
+   public void Dispose()
+   {
+   		// implementation
+   }
+}
+
+// #6
+class Program
+{
+    private enum MegaEnum
+    {
+        First = 15,
+        Second,
+        Third = 54
+    }
+  
+    static void Main()
+    {
+        Console.WriteLine((int)MegaEnum.Second);
+        Console.Read();
+    }
 }
