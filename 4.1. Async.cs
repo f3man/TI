6bf1fb66c7ddb01801ab2cfc13c5e1c081b5077b
Stepan Chubanian <Stepan.Chubanian@matcotools.com>
2024-02-26 11:23:22 +0200

    [add] ef and remove multithreading

--- 4.1. Async.cs
+++ 4.1. Async.cs
@@ -31,19 +31,3 @@ public class Program
 
 
 }
-
-
-public class AddressRepository
-{
-    public DbSet<Address> Addresses { get; set; }
-
-    public async Task<List<Address>> GetAddresses()
-    {
-        var addresses1 = await Addresses.AsQueryable()
-            .Where(x => x.Country == "Ukraine");
-
-        var addresses2 = await Addresses.AsEnumerable()
-            .Where(x => x.Country == "Moldova");
-
-    }
-}
